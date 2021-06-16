9483f54ec8e23f4e87e0567b03aff2cb41f2d4b6
phulinMN <pongpaiMN@gmail.com>
2021-06-16 15:12:34 +0700

    change scss to sass

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
 			// @/ is an alias to src/
 			// so this assumes you have a file named `src/variables.sass`
 			// Note: this option is named as "prependData" in sass-loader v8
-			scss: {
+			sass: {
 				additionalData: `@import "@/assets/scss/main.scss";`
 			},
 		}
